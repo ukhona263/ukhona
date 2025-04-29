01706eaf48253ff4502728d3ebed81ccd96a1f1e
ukhona263 <washingtonbubi@gmail.com>
2025-04-29 09:03:38 +0200

    Add files via upload

---
+++ speedstar motors/Untitled-1.java
@@ -0,0 +1,17 @@
+public void setStudentName(int newAge){//Mutator for student’s name
+this. studentAge= newAge;
+}
+public void setSchoolName(String schoolName){//Mutator for schoolName
+this. schoolName= schoolName;
+}
+public void setCourseName(String courseName){//Mutator for courseName
+this. courseName=courseName;
+}
+public String getSchoolName(){//Accessor for schoolName
+   return schoolName;
+}
+public String getCourseName(){//Accesor for courseName
+   return courseName;
+}
+}   
+
